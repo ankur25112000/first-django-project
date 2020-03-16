5dd559ca1a7f5b6e0ab17821318694c2617a501e
ankur25112000 <ankursingh2025@gmail.com>
2020-03-17 01:56:28 +0530

    all done ready to deploy

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    title = models.CharField(max_length = 500)
+    title = models.TextField()
     pub_date = models.DateTimeField(null=True)
     image = models.ImageField(upload_to='image/')
     body = models.TextField()
+
+def __str__(self):
+    return self.title
+
+def summary(self):
+    return self.body[ :100]
